750eea5f69cdfe7ca60a6ce62f362e16789f270f
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-12-10 21:47:08 +0530

    complete

--- Calculator/Styles.js
+++ Calculator/Styles.js
@@ -1,11 +1,12 @@
 import { StyleSheet } from 'react-native'
 
 export default Exstyles = StyleSheet.create({
+
     container: {
         flex: 1,
-        backgroundColor: 'white',
+        backgroundColor: 'black',
+        alignItems: 'center',
         justifyContent: 'center',
-        alignItems: 'center'
     },
     displayContainer: {
         flex: 2,
@@ -15,30 +16,58 @@ export default Exstyles = StyleSheet.create({
     },
     displayText: {
         fontSize: 48,
-        color: 'black'
+        color: 'white',
     },
     buttonContainer: {
         flex: 3,
-        width: '80%'
+        width: '100%',
     },
     row: {
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginBottom: 10
+        marginBottom: 10,
     },
     button: {
         flex: 1,
-        borderRadius: 50,
+        borderRadius: 30,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#fff',
-        elevation: 3,
-        margin: 2,
+        backgroundColor: 'white',
+        elevation: 5,
+        margin: 1,
         padding: 15,
     },
     buttonText: {
-        fontSize: 34,
-        color: '#333',
+        fontSize: 36,
+        color: 'black',
+    },
+    zeroButton: {
+        flex: 1,
+        padding: 15
+    },
+    zeroButtonText: {
+        fontSize: 36,
+        color: 'black',
+    },
+    operatorButton: {
+        backgroundColor: '#eb8744',
+    },
+    operatorButtonText: {
+        color: 'black',
+    },
+    equalButton: {
+        flex: 1,
+        borderRadius: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#eb8744',
+        elevation: 5,
+    },
+    equalButtonText: {
+        color: 'black',
+    },
+    clearButtonText: {
+        color: 'black',
     },
 })
