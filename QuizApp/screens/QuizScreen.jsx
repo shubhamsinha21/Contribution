7a7406c96abd9c55ac499e574ff7b1b70ee4b89e
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2024-01-14 14:12:11 +0530

    quiz screen 👉🏻

--- QuizApp/screens/QuizScreen.jsx
+++ QuizApp/screens/QuizScreen.jsx
@@ -1,7 +1,5 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-
-import React, { useEffect, useState } from 'react'
-
+import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 const shuffle = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -12,72 +10,90 @@ const shuffle = (array) => {
 };
 
 export default function QuizScreen({ navigation }) {
-
     const [questions, setQuestions] = useState();
     const [questionNumber, setQuestionNumber] = useState(0);
-    const [options, setOptions] = useState([])
-    const [score, setScore] = useState(0)
+    const [options, setOptions] = useState([]);
+    const [score, setScore] = useState(0);
+    const [selectedOption, setSelectedOption] = useState(null)
 
     const getData = async () => {
-        const url = 'https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple&encode=url3986';
+        const url = 'https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple&encode=url3986';
         const res = await fetch(url);
         const data = await res.json();
-        setQuestions(data.results)
-        setOptions(generateShuffledOptions(data.results[0]))
-    }
+        setQuestions(data.results);
+        setOptions(generateShuffledOptions(data.results[0]));
+    };
 
     useEffect(() => {
-        getData()
-    }, [])
+        getData();
+    }, []);
 
+    const handleNext = () => {
+        setQuestionNumber(questionNumber + 1);
+        setOptions(generateShuffledOptions(questions[questionNumber + 1]));
+    };
 
-    const handleNextPress = () => {
-        setQuestionNumber(questionNumber + 1)
-        setOptions(generateShuffledOptions(questions[questionNumber + 1]))
+    const handlePrevious = () => {
+        setQuestionNumber(questionNumber - 1);
+        setOptions(generateShuffledOptions(questions[questionNumber - 1]));
     }
 
-
     const generateShuffledOptions = (_questions) => {
-        const options = [..._questions.correct_answer]
-        options.push(_questions.correct_answer)
-        shuffle(options)
-        return options
+        const options = [..._questions.incorrect_answers];
+        options.push(_questions.correct_answer);
+        shuffle(options);
+        return options;
+    };
+
+    const handleSelectedOption = (_option) => {
+        console.log(_option === questions[questionNumber].correct_answer);
+        if (_option === questions[questionNumber].correct_answer) setScore(score + 10);
+        setSelectedOption(_option)
+    };
+
+    const handleQuizSubmission = () => {
+        navigation.navigate('Result', { score })
     }
     return (
         <>
             {questions && (
                 <View>
                     <View className='mx-1 my-16'>
-
-                        <Text className='text-2xl font-semibold text-black'>Q{questionNumber + 1}. {decodeURIComponent(questions[questionNumber].question)}</Text>
+                        <Text className='text-2xl font-semibold text-black'>
+                            Q{questionNumber + 1}. {decodeURIComponent(questions[questionNumber].question)}
+                        </Text>
                     </View>
                     <View className='my-8 space-y-3 mx-2 '>
-                        <TouchableOpacity><Text className='bg-yellow-700 text-white px-2 py-2 mx-2 text-lg rounded-lg'>{decodeURIComponent(options[0])}</Text></TouchableOpacity>
-
-                        <TouchableOpacity><Text className='bg-yellow-700 text-white px-2 py-2 mx-2 text-lg rounded-lg'>{decodeURIComponent(options[1])}</Text></TouchableOpacity>
-
-                        <TouchableOpacity><Text className='bg-yellow-700 text-white px-2 py-2 mx-2 text-lg rounded-lg'>{decodeURIComponent(options[2])}</Text></TouchableOpacity>
-
-                        <TouchableOpacity><Text className='bg-yellow-700 text-white px-2 py-2 mx-2 text-lg rounded-lg'>{decodeURIComponent(options[3])}</Text></TouchableOpacity>
+                        {options.map((option, index) => (
+                            <TouchableOpacity key={index} onPress={() => handleSelectedOption(option)}
+                            >
+                                <Text
+                                    style={{ backgroundColor: selectedOption !== option ? '#996400' : 'red' }}
+                                    className='text-white px-2 py-2 mx-2 text-lg rounded-lg'>
+                                    {decodeURIComponent(option)}
+                                </Text>
+                            </TouchableOpacity>
+                        ))}
                     </View>
                     <View className='flex flex-row justify-around my-24'>
-                        <TouchableOpacity onPress={() => navigation.navigate('Home')} >
-                            <Text className='text-white rounded-lg p-3 text-xl bg-yellow-700 w-28 text-center'>Previous</Text>
-                        </TouchableOpacity>
+                        {questionNumber > 1(
+                            <TouchableOpacity onPress={handlePrevious}>
+                                <Text className='text-white rounded-lg p-3 text-xl bg-yellow-700 w-28 text-center'>Previous</Text>
+                            </TouchableOpacity>
+                        )}
 
                         {questionNumber < 9 ? (
-                            <TouchableOpacity onPress={handleNextPress}>
+                            <TouchableOpacity onPress={handleNext}>
                                 <Text className='text-white rounded-lg p-3 text-xl bg-yellow-700 w-28 text-center'>Next</Text>
                             </TouchableOpacity>
                         ) : (
-                            <TouchableOpacity >
+                            <TouchableOpacity onPress={handleQuizSubmission}>
                                 <Text className='text-white rounded-lg p-3 text-xl bg-yellow-700 w-28 text-center'>Submit</Text>
                             </TouchableOpacity>
                         )}
                     </View>
                 </View>
             )}
-
         </>
-    )
+    );
 }
--- QuizApp/screens/ResultScreen.jsx
+++ QuizApp/screens/ResultScreen.jsx
@@ -1,10 +1,12 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 
-export default function ResultScreen() {
+export default function ResultScreen({ route }) {
+
+    const { score } = route.params;
     return (
         <>
-            <Text className='text-3xl font-bold text-black'>Result</Text>
+            <Text className='text-3xl font-bold text-black'>Result:{score}</Text>
             <View className='flex-1 items-center'>
                 <Image source={require('../assets/home-image.png')}
                     className="w-96 h-96 my-8"
