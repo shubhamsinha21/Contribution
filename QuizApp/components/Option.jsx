21cae430b236d2d4845a0594a70f2310e90845d2
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2024-01-14 11:59:39 +0530

    logic

--- QuizApp/components/Option.jsx
+++ QuizApp/components/Option.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 export default function Option() {
     return (
-        <View className='my-8 space-y-3 flex-1'>
-            <TouchableOpacity><Text className='bg-yellow-700 text-white px-2 py-2 mx-2 text-lg w-56 rounded-lg'>Text</Text></TouchableOpacity>
+        <View className='my-8 space-y-3 flex-1 mx-2 '>
+            <TouchableOpacity><Text className='bg-yellow-700 text-white px-2 py-2 mx-2 text-lg rounded-lg'>Text</Text></TouchableOpacity>
 
-            <TouchableOpacity><Text className='bg-yellow-700 text-white px-2 py-2 mx-2 text-lg w-56 rounded-lg'>Text</Text></TouchableOpacity>
+            <TouchableOpacity><Text className='bg-yellow-700 text-white px-2 py-2 mx-2 text-lg rounded-lg'>Text</Text></TouchableOpacity>
 
-            <TouchableOpacity><Text className='bg-yellow-700 text-white px-2 py-2 mx-2 text-lg w-56 rounded-lg'>Text</Text></TouchableOpacity>
+            <TouchableOpacity><Text className='bg-yellow-700 text-white px-2 py-2 mx-2 text-lg rounded-lg'>Text</Text></TouchableOpacity>
 
-            <TouchableOpacity><Text className='bg-yellow-700 text-white px-2 py-2 mx-2 text-lg w-56 rounded-lg'>Text</Text></TouchableOpacity>
+            <TouchableOpacity><Text className='bg-yellow-700 text-white px-2 py-2 mx-2 text-lg rounded-lg'>Text</Text></TouchableOpacity>
         </View>
     )
 }
