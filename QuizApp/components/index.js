32db25d745c863382e05fc9546aa6e0825f4b1ee
Shubham Sinha <84564814+shubhamsinha21@users.noreply.github.com>
2024-01-17 17:51:12 +0530

    Delete QuizApp directory

--- QuizApp/components/index.js
+++
@@ -1,5 +0,0 @@
-import Title from "./Title";
-import Banner from "./Banner";
-import Button from "./Button";
-
-export { Title, Banner, Button }
--- QuizApp/screens/index.js
+++
@@ -1,5 +0,0 @@
-import HomeScreen from './HomeScreen'
-import QuizScreen from './QuizScreen'
-import ResultScreen from './ResultScreen'
-
-export { HomeScreen, QuizScreen, ResultScreen }
